7b55fe62212a2942f206630db6633be5167e031b
jonas ja <jonas774@outlook.com>
2021-06-07 12:56:49 -0500

    Changed to production compilation

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -6,30 +6,34 @@ class DocsController < ApplicationController
   end
 
   def create
-    uploaded = params[:file]
-    File.open("./tmp/csv_doc.csv", 'w') do |file|
-      file.write(uploaded.read)
-    end
+    begin
+      uploaded = params[:file]
+      File.open("./tmp/csv_doc.csv", 'w') do |file|
+        file.write(uploaded.read)
+      end
 
-    lines = SmarterCSV.process('./tmp/csv_doc.csv')
-    @new_hash = lines
+      lines = SmarterCSV.process('./tmp/csv_doc.csv')
+      @new_hash = lines
 
-    lines.each_with_index do |l1, i|
-      @new_hash.each_with_index do |l2, j|
-        if i != j
-          if (l1[:nombre].to_s == l2[:nombre].to_s) || (l1[:correo_electronico].to_s == l2[:correo_electronico].to_s) || (l1[:telefono].to_s == l2[:telefono].to_s)
-            @new_hash[j][:flag] = 0
-          end
+      lines.each_with_index do |l1, i|
+        @new_hash.each_with_index do |l2, j|
+          if i != j
+            if (l1[:nombre].to_s == l2[:nombre].to_s) || (l1[:correo_electronico].to_s == l2[:correo_electronico].to_s) || (l1[:telefono].to_s == l2[:telefono].to_s)
+              @new_hash[j][:flag] = 0
+            end
 
-          if is_valid_email?(l2[:correo_electronico]).nil?
-            @new_hash.delete(l2)
-          end
+            if is_valid_email?(l2[:correo_electronico]).nil?
+              @new_hash.delete(l2)
+            end
 
-          if (l1[:nombre].to_s == l2[:nombre].to_s) && (l1[:correo_electronico].to_s == l2[:correo_electronico].to_s) && (l1[:telefono].to_s == l2[:telefono].to_s)
-            @new_hash.delete(l2)
+            if (l1[:nombre].to_s == l2[:nombre].to_s) && (l1[:correo_electronico].to_s == l2[:correo_electronico].to_s) && (l1[:telefono].to_s == l2[:telefono].to_s)
+              @new_hash.delete(l2)
+            end
           end
         end
       end
+    rescue
+      redirect root_path
     end
   end
 

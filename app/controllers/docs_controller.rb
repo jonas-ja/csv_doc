a1d0b2a1d27996bd35a8c5c2d01422f8a1a09300
jonas ja <jonas774@outlook.com>
2021-06-07 14:58:28 -0500

    final

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -6,7 +6,7 @@ class DocsController < ApplicationController
   end
 
   def create
-    begin
+    # begin
       uploaded = params[:file]
       File.open("./tmp/csv_doc.csv", 'w') do |file|
         file.write(uploaded.read)
@@ -15,11 +15,13 @@ class DocsController < ApplicationController
       lines = SmarterCSV.process('./tmp/csv_doc.csv')
       @new_hash = lines
 
-      lines.each_with_index do |l1, i|
-        @new_hash.each_with_index do |l2, j|
-          if i != j
+      lines.each_with_index do |l1, index1|
+        @new_hash.each_with_index do |l2, index2|
+          if index1 != index2
             if (l1[:nombre].to_s == l2[:nombre].to_s) || (l1[:correo_electronico].to_s == l2[:correo_electronico].to_s) || (l1[:telefono].to_s == l2[:telefono].to_s)
-              @new_hash[j][:flag] = 0
+
+              @new_hash[index1][:flag] = 0
+              @new_hash[index2][:flag] = 0
             end
 
             if is_valid_email?(l2[:correo_electronico]).nil?
@@ -27,14 +29,16 @@ class DocsController < ApplicationController
             end
 
             if (l1[:nombre].to_s == l2[:nombre].to_s) && (l1[:correo_electronico].to_s == l2[:correo_electronico].to_s) && (l1[:telefono].to_s == l2[:telefono].to_s)
+
               @new_hash.delete(l2)
+
             end
           end
         end
       end
-    rescue
-      redirect root_path
-    end
+    # rescue
+    #   redirect_to root_path
+    # end
   end
 
   def is_valid_email? email
